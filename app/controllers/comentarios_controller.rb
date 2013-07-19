dcd8ff23ddedb1fa3d3f230105e260e95715cdd2
Zyon Dias <zyon.dias@hotmail.com>
2013-07-19 20:00:41 -0300

    Aula 19

--- app/controllers/comentarios_controller.rb
+++ app/controllers/comentarios_controller.rb
@@ -46,6 +46,7 @@ class ComentariosController < ApplicationController
       if @comentario.save
         format.html { redirect_to @comentario, notice: 'Comentario was successfully created.' }
         format.json { render json: @comentario, status: :created, location: @comentario }
+        format.js
       else
         format.html { render action: "new" }
         format.json { render json: @comentario.errors, status: :unprocessable_entity }
